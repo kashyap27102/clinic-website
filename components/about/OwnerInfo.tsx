60b0fb291e72eb7a48fc1de3e68ef1a23d370119
Kashyap Patel <ksnakrani5544@gmail.com>
2024-06-10 23:38:14 +0530

    adjust font and gap on about us page

--- components/about/OwnerInfo.tsx
+++ components/about/OwnerInfo.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
-import OwnerImg from "@/public/owner.jpg"
+import OwnerImg from "@/public/owner.jpg";
 
 export const OwnerInfo = () => {
   return (
     <>
       <div className="text-center mb-12">
-        <p className="text-primaryDark uppercase tracking-widest">
-          MEET OUR
-        </p>
+        <p className="text-primaryDark uppercase tracking-widest">MEET OUR</p>
         <h2 className="text-3xl lg:text-4xl font-semibold text-dark mt-2">
           Expert Physiotherapy Doctor
         </h2>
@@ -20,16 +18,16 @@ export const OwnerInfo = () => {
               (Consultant physiotherapist)
             </h5>
           </div>
-          <p className="text-lg">
+          <p className="text-base text-justify">
             Dr. Prahyuman Patel is a highly qualified and experienced
-            physiotherapist in Gandhinagar, with more than 2 years of experience in
-            the field. He is providing quality care to patients of all ages, ranging
-            from children to elders. In addition to his clinical work, he is
-            also the director of World of Rehab, a leading physiotherapy clinic
-            in Gandhinagar that provides innovative and effective treatments to
-            patients in need.
+            physiotherapist in Gandhinagar, with more than 2 years of experience
+            in the field. He is providing quality care to patients of all ages,
+            ranging from children to elders. In addition to his clinical work,
+            he is also the director of World of Rehab, a leading physiotherapy
+            clinic in Gandhinagar that provides innovative and effective
+            treatments to patients in need.
           </p>
-          <p className="text-lg">
+          <p className="text-base text-justify">
             Dr. Pradhyuman is known for his commitment, compassion, and friendly
             demeanor, which have earned him a reputation as a dedicated and
             caring physiotherapist. He is passionate about helping people of all
@@ -39,7 +37,7 @@ export const OwnerInfo = () => {
           </p>
         </div>
         <div className="col-span-1">
-          <Image src={OwnerImg} alt="owner-info" className="rounded-e-md"/>
+          <Image src={OwnerImg} alt="owner-info" className="rounded-e-md" />
         </div>
       </div>
     </>
